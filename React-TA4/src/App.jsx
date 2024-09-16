8a6dd4c281fda7683167d8253e5b583ad478786c
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-09-16 19:57:01 -0300

    a

---
+++ React-TA4/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter/Counter";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='app'>
+        <h1>Contador</h1>
+        <Counter />
+      </div>
+    </>
+  )
+}
+
+export default App
